6e3ccc908fba8bd45e2f9cf9efe8ed4a504ef67b
Rutger <1201954@student.roc-nijmegen.nl>
2024-10-03 16:21:18 +0200

    Write docs for swagger

--- back-end/app/Http/Controllers/HealthController.php
+++ back-end/app/Http/Controllers/HealthController.php
@@ -9,12 +9,30 @@ class HealthController extends Controller
 {
 
     /**
-     * @OA\Info(
-     *     version="1.0",
-     *     title="Example for response examples value"
-     * ),
-     * @OA\PathItem(
-     *     path="/health"
+     * @OA\Info (
+     *          version="0.1",
+     *          title="API Documentation",
+     * )
+     * @OA\Get(
+     *     path="api/health",
+     *     tags={"Health"},
+     *     summary="Health check",
+     *     description="Health check",
+     *     @OA\Response(
+     *         response=200,
+     *         description="Health check",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="status",
+     *                 type="string",
+     *                 example="ok"
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=500,
+     *         description="Internal Server Error"
+     *     )
      * )
      */
     public function index()
--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -5,6 +5,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Health;
 use App\Http\Controllers\HealthController;
 
+/**
+ * @OA\Info(
+ *     version="0.1",
+ *     title="API Documentation",
+ * ),
+ */
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
