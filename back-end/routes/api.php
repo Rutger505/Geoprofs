d503f69c8aaf739a028e6c137fea65518b290a85
ineedausernamehelp <54716860+JakevdKolk@users.noreply.github.com>
2024-10-11 10:50:53 +0200

    got rid of temp controller

--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -3,7 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HealthController;
-use App\Http\Controllers\UserTempController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -15,4 +14,3 @@ Route::get('/', function () {
 
 
 Route::get('/health', [HealthController::class, 'index']);
-Route::get('/userTemp', [UserTempController::class, 'index']);
