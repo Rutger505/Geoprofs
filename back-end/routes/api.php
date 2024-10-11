f074e464c9b40d4aeb32716f62cfb16d7eb5fed4
ineedausernamehelp <54716860+JakevdKolk@users.noreply.github.com>
2024-10-11 11:42:31 +0200

    copied rutgers auth from previous project

--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -3,6 +3,16 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HealthController;
+use App\Http\Controllers\AuthController;
+
+Route::prefix('auth')->group(function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('logout', [AuthController::class, 'logout']);
+    });
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();
