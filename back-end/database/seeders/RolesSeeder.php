4559fffda33e22f03849fb7c5657f5e5296efcb8
ineedausernamehelp <54716860+JakevdKolk@users.noreply.github.com>
2024-10-11 11:02:08 +0200

    added te rest of the roles

--- back-end/database/seeders/RolesSeeder.php
+++ back-end/database/seeders/RolesSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use App\Models\Roles;
 
 class RolesSeeder extends Seeder
 {
@@ -13,12 +14,24 @@ class RolesSeeder extends Seeder
      */
     public function run(): void
     {
+        if (Roles::where('RoleID', 1)->exists()) {
+            return;
+        }
         DB::table('roles')->insert([
             [
                 'RoleName' => 'CEO'
             ],
             [
                 'RoleName' => 'Employee'
+            ],
+            [
+                'RoleName' => 'Admin'
+            ],
+            [
+                'RoleName' => 'SectionManager'
+            ],
+            [
+                'RoleName' => 'ProjectManager'
             ]
         ]);
     }
