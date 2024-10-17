5a8171f38b247fe60f25a627a327c22f3441a2d9
Rutger <1201954@student.roc-nijmegen.nl>
2024-10-17 12:27:05 +0200

    Make logout post

--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -10,7 +10,7 @@ Route::prefix('auth')->group(function (): void {
     Route::post('/login', [AuthController::class, 'login']);
 
     Route::middleware('auth:sanctum')->group(function (): void {
-        Route::get('logout', [AuthController::class, 'logout']);
+        Route::post('logout', [AuthController::class, 'logout']);
     });
 });
 
