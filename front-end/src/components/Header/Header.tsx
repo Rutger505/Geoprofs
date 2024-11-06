6789a4fe91aaea3bf75226588c60b0ad031d1a30
Rutger Pronk <rutger.pronk11@gmail.com>
2024-11-06 14:14:35 +0100

    Start working on header component

--- front-end/src/components/Header/Header.tsx
+++ front-end/src/components/Header/Header.tsx
@@ -1,44 +1,28 @@
-import { HeaderLink } from "@/components/Header/HeaderLink";
-import { MobileNavigation } from "@/components/Header/MobileNavigation";
-import Image from "next/image";
-import Link from "next/link";
-
-export interface Navigation {
-  name: string;
-  href: string;
-}
-
 export const navigation = [
-  { name: "Dashboard", href: "/dashboard" },
-  { name: "Aanvragen", href: "#" },
-  { name: "Verlofsaldo", href: "#" },
+  { name: "Product", href: "#" },
+  { name: "Features", href: "#" },
+  { name: "About", href: "#" },
+  { name: "Contact", href: "#" },
 ];
 
 export function Header() {
   return (
-    <div className={"flex w-full justify-center"}>
-      <header className="flex h-20 w-full max-w-7xl items-center justify-between px-20">
-        <div>
-          <Link href="/">
-            <Image
-              src="/logo.webp"
-              alt="Geoprofs Logo"
-              width={42}
-              height={42}
-            />
-          </Link>
-        </div>
+    <div className="flex items-center justify-between py-6">
+      <div className="flex justify-start">
+        <span className="text-2xl font-bold">sdfsdfsdf</span>
+      </div>
 
-        <div className={"hidden gap-5 md:flex"}>
-          {navigation.map((item) => (
-            <HeaderLink name={item.name} href={item.href} key={item.name} />
-          ))}
-        </div>
-
-        <div className={"md:hidden"}>
-          <MobileNavigation navigation={navigation} />
-        </div>
-      </header>
+      <div className="hidden md:flex items-center space-x-8">
+        {navigation.map((item) => (
+          <a
+            key={item.name}
+            href={item.href}
+            className="text-gray-500 hover:text-gray-900"
+          >
+            {item.name}
+          </a>
+        ))}
+      </div>
     </div>
   );
 }
