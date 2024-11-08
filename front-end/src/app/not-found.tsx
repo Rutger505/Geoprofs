45e21badc4d0addbe0b65af6a0989225838617a5
Rutger Pronk <rutger.pronk11@gmail.com>
2024-11-08 11:14:09 +0100

    Create notfound

---
+++ front-end/src/app/not-found.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="flex items-center justify-center">
+      <div className={"text-center"}>
+        <div className={"mb-10 flex gap-4"}>
+          <h1 className="text-5xl font-bold text-gray-900">404</h1>
+
+          <div className="h-50 w-px border-l-2 border-gray-300" />
+
+          <h2 className="my-auto text-2xl font-semibold text-gray-700">
+            Page Not Found
+          </h2>
+        </div>
+
+        <Link href="/">Go back home</Link>
+      </div>
+    </div>
+  );
+}
