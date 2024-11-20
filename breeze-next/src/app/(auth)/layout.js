7a3ce789d0a7d805742cd735040eb435185059f1
Rutger Pronk <rutger.pronk11@gmail.com>
2024-11-20 21:16:53 +0100

    Format and remove email logic from frontend

--- breeze-next/src/app/(auth)/layout.js
+++ breeze-next/src/app/(auth)/layout.js
@@ -3,24 +3,25 @@ import AuthCard from '@/app/(auth)/AuthCard'
 import ApplicationLogo from '@/components/ApplicationLogo'
 
 export const metadata = {
-    title: 'Laravel',
+  title: 'Laravel',
 }
 
-const Layout = ({ children }) => {
-    return (
-        <div>
-            <div className="text-gray-900 antialiased">
-                <AuthCard
-                    logo={
-                        <Link href="/">
-                            <ApplicationLogo className="w-20 h-20 fill-current text-gray-500" />
-                        </Link>
-                    }>
-                    {children}
-                </AuthCard>
-            </div>
-        </div>
-    )
+const Layout = ({children}) => {
+  return (
+    <div>
+      <div className="text-gray-900 antialiased">
+        <AuthCard
+          logo={
+            <Link href="/">
+              <ApplicationLogo className="w-20 h-20 fill-current text-gray-500"/>
+            </Link>
+          }>
+          {children}
+        </AuthCard>
+      </div>
+    </div>
+  )
 }
 
 export default Layout
+
