3258cb40ba2354f32a3e8410ce173bcc62d58eba
Rutger Pronk <rutger.pronk11@gmail.com>
2024-11-21 09:43:29 +0100

    Fix sytnax

--- back-end/bootstrap/app.php
+++ back-end/bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        StartSession::class,
+        StartSession::class;
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
