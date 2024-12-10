42ba61b78805b520dc559af427daa586c3aa1d89
ineedausernamehelp <54716860+JakevdKolk@users.noreply.github.com>
2024-12-10 12:10:22 +0100

    contract maken route gemaakt

--- back-end/app/Http/Controllers/ContractController.php
+++ back-end/app/Http/Controllers/ContractController.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Contracts;
 use Illuminate\Http\Request;
 
 class ContractController extends Controller
 {
     //
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'contract_name' => 'string',
+            'contract_leave_hours' => 'int'
+        ]);
+
+        Contracts::create([
+            'ContractName' => $request['contract_name'],
+            'ContractTotalLeaveHours' => $request['contract_leave_hours']
+        ]);
+
+        return response()->json(['message' => 'Contract created'], 200);
+    }
+
+    public function get()
+    {
+        $contracts = Contracts::get('*');
+
+        return response()->json(['message' => $contracts]);
+    }
 }
--- back-end/app/Http/Middleware/EnsureUserIsAdmin.php
+++ back-end/app/Http/Middleware/EnsureUserIsAdmin.php
@@ -8,20 +8,23 @@ use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Support\Facades\Auth;
 
-
 class EnsureUserIsAdmin
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function handle(Request $request, Closure $next): Response
     {
         $user = Auth::user();
 
-        if ($user['RoleID'] == 3) {
-            return null;
+        $role = Roles::where('RoleID', $user['UserRoleID'])->first();
+
+        if (!$role || $role['RoleName'] !== 'Admin') {
+            return response()->json(['error' => 'Unauthorized'], 403);
         }
 
         return $next($request);
