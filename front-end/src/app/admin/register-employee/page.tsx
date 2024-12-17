689b56592d6989a6aed6dd1a47dbdf0589c61598
ineedausernamehelp <54716860+JakevdKolk@users.noreply.github.com>
2024-12-17 10:39:25 +0100

    phpstorm switch

--- front-end/src/app/admin/register-employee/page.tsx
+++ front-end/src/app/admin/register-employee/page.tsx
@@ -1,25 +1,42 @@
+import axios from "axios";
+
+interface roles {
+  id: number
+}
+
 export default function Page() {
+
+
+
+const getRoles = async (): Promise
+
+
+  const { data } = axios
+    .get("https://localhost/api/roles/show")
+    .then((response) => {
+      console.log(response.data);
+    });
+
   return (
     <main className="flex items-center justify-center">
+      <div>
+        <h1>Account aanmaken voor</h1>
+
+        <form action="">
+          <input type="text" name="email" id="" />
+
           <div>
-            <h1>Account aanmaken voor</h1>
+            <input type="text" name="first_name" id="" />
+            <input type="text" name="last_name" id="" />
+          </div>
 
-            <form action="">
-              <input type="text" name="email" id="" />
+          <input type="date" />
 
-              <div>
-                <input type="text" name="first_name" id="" />
-                <input type="text" name="last_name" id="" />
-              </div>
-
-              <input type="date" />
-
-              <select name="account type" id="">
-                <option value="">werknemer</option>
-              </select>
-
-            </form>
-          </div>      
+          <select name="account type" id="">
+            <option value="">werknemer</option>
+          </select>
+        </form>
+      </div>
     </main>
   );
 }
