d1fda1c8b3c3df52de7a7d5ad49bc86e6a0100a3
Rutger Pronk <rutger.pronk11@gmail.com>
2024-12-18 13:32:09 +0100

    Create complete account form

--- front-end/src/lib/authActions.ts
+++ front-end/src/lib/authActions.ts
@@ -2,6 +2,10 @@
 
 import { signIn, signOut } from "@/lib/auth";
 import { ApiResponseError } from "@/lib/errors";
+import axios from "@/lib/axios";
+import { redirect } from "next/navigation";
+
+const MIN_PASSWORD_LENGTH = 8;
 
 export async function login(email: string, password: string) {
   try {
@@ -22,4 +26,29 @@ export async function login(email: string, password: string) {
 
 export async function logout(): Promise<void> {
   await signOut({ redirect: true, redirectTo: "/" });
+}
+
+export async function activateAccount(
+  password: string,
+  repeatPassword: string,
+) {
+  if (password !== repeatPassword) {
+    throw new Error("Passwords do not match");
+  }
+
+  if (password.length < MIN_PASSWORD_LENGTH) {
+    throw new Error("Password must be at least 8 characters long");
+  }
+
+  try {
+    await axios.post("/api/auth/activate", {
+      password,
+    });
+  } catch (e) {
+    // do something proper error handling
+    console.error(e);
+    throw new Error("Something went wrong");
+  }
+
+  redirect("/dashboard");
 }
