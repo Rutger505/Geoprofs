faefe3b6d7e401001177bc1314bccfc1a1d5928e
Rutger Pronk <rutger.pronk11@gmail.com>
2024-12-24 09:53:27 +0100

    Improve env.example description, add configuration for kubernetes deployments

--- front-end/src/env.ts
+++ front-end/src/env.ts
@@ -5,6 +5,7 @@ export const env = createEnv({
   server: {
     NODE_ENV: z.enum(["development", "production", "test"]),
     BACKEND_URL: z.string().url(),
+    AUTH_SECRET: z.string(),
   },
   client: {
     NEXT_PUBLIC_BACKEND_URL: z.string().url(),
@@ -13,6 +14,7 @@ export const env = createEnv({
     NODE_ENV: process.env.NODE_ENV,
     BACKEND_URL: process.env.BACKEND_URL,
     NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
+    AUTH_SECRET: process.env.AUTH_SECRET,
   },
   skipValidation: !!process.env.SKIP_ENV_VALIDATION,
 });
