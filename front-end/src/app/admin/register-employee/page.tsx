65bec57e7a9975505a48fcaf6141c2bd249782b2
Rutger Pronk <rutger.pronk11@gmail.com>
2025-01-01 15:21:00 +0100

    Start creating register employee form

--- front-end/src/app/admin/register-employee/page.tsx
+++ front-end/src/app/admin/register-employee/page.tsx
@@ -1,5 +1,17 @@
+import { RegisterEmployeeForm } from "@/components/Admin/RegisterEmployee/RegisterEmployeeForm";
+
 export default function Page() {
+  // TODO get roles from backend
+  const roles = [
+    { name: "Admin", id: 1 },
+    { name: "Employee", id: 2 },
+  ];
+
   return (
-    <main className="flex items-center justify-center">Register employee</main>
+    <main className="flex items-center justify-center">
+      <h1>Register employee</h1>
+
+      <RegisterEmployeeForm roles={roles} defaultRole={roles[1]} />
+    </main>
   );
 }
---
+++ front-end/src/components/Admin/RegisterEmployee/RegisterEmployeeForm.tsx
@@ -0,0 +1,115 @@
+"use client";
+
+import { Button, Field, Input, Label, Select } from "@headlessui/react";
+import { useMutation } from "@tanstack/react-query";
+import clsx from "clsx";
+import { isRedirectError } from "next/dist/client/components/redirect";
+import { useState } from "react";
+
+interface Role {
+  name: string;
+  id: number;
+}
+
+interface Props {
+  roles: Role[];
+  defaultRole: Role;
+}
+
+export function RegisterEmployeeForm({ roles, defaultRole }: Readonly<Props>) {
+  const { mutate, isPending, error } = useMutation({
+    mutationFn: async () => {
+      console.error("Not implemented");
+      throw new Error("Not implemented");
+    },
+  });
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [dateHired, setDateHired] = useState("");
+  const [role, setRole] = useState<Role>(defaultRole);
+
+  return (
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        mutate();
+      }}
+      className="space-y-6"
+    >
+      <Field>
+        <Label className={"block"}>Voornaam</Label>
+        <Input
+          className="w-full rounded-md border border-gray-300 px-3 py-2 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-500"
+          onChange={(event) => setFirstName(event.target.value)}
+          required
+        />
+      </Field>
+
+      <Field>
+        <Label className={"block"}>Achternaam</Label>
+        <Input
+          className="w-full rounded-md border border-gray-300 px-3 py-2 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-500"
+          onChange={(event) => setLastName(event.target.value)}
+          required
+        />
+      </Field>
+
+      <Field>
+        <Label className={"block"}>Email</Label>
+        <Input
+          className="w-full rounded-md border border-gray-300 px-3 py-2 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-500"
+          onChange={(event) => setEmail(event.target.value)}
+          required
+        />
+      </Field>
+
+      <Field>
+        <Label className={"block"}>Datum in dienst</Label>
+        <Input
+          className="w-full rounded-md border border-gray-300 px-3 py-2 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-500"
+          onChange={(event) => setDateHired(event.target.value)}
+          type="date"
+          required
+        />
+      </Field>
+
+      <Field>
+        <Label className={"block"}>Rol</Label>
+        <Select
+          className="w-full rounded-md border border-gray-300 px-3 py-2 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-500"
+          onChange={(event) =>
+            setRole(roles.find((role) => role.id === +event.target.value)!)
+          }
+          aria-label="Rol"
+        >
+          {roles.map((role) => (
+            <option key={role.id} value={role.id}>
+              {role.name}
+            </option>
+          ))}
+        </Select>
+      </Field>
+
+      <div className="space-y-4">
+        <Button
+          type="submit"
+          className={clsx(
+            "w-full rounded-md bg-blue-500 px-4 py-2 text-white transition-colors hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2",
+            isPending && "cursor-not-allowed opacity-50",
+          )}
+        >
+          {isPending ? "Inloggen..." : "Inloggen"}
+        </Button>
+      </div>
+
+      {error && !isRedirectError(error) ? (
+        <p className="mt-1 text-sm text-red-600">{error.message}</p>
+      ) : (
+        <p className="invisible mt-1 text-sm text-red-600" aria-hidden="true">
+          spacer
+        </p>
+      )}
+    </form>
+  );
+}
