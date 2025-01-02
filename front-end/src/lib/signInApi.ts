76d44023aff32c05cc0dc306e0461f124a95f6c2
Rutger Pronk <rutger.pronk11@gmail.com>
2025-01-02 18:42:08 +0100

    Simplify authorize to match new simple endpoint

--- front-end/src/lib/signInApi.ts
+++ front-end/src/lib/signInApi.ts
@@ -1,16 +1,10 @@
 "use server";
 import axios from "@/lib/axios";
-import { ApiResponseError } from "@/lib/errors";
 import { AxiosError } from "axios";
-import { cookies } from "next/headers";
 
 interface AuthorizeCredentials {
   email: unknown;
   password: unknown;
-}
-
-interface ApiLoginResponse {
-  user: ApiUser;
 }
 
 export interface ApiUser {
@@ -27,29 +21,8 @@ export interface ApiUser {
 
 export async function authorize(credentials: Partial<AuthorizeCredentials>) {
   try {
-    const loginResponse = await axios.post<ApiLoginResponse>(
-      "/auth/login",
-      credentials,
-    );
-
-    const responseCookies = loginResponse.headers["set-cookie"];
-    if (!responseCookies) {
-      throw new Error("No session cookies in response");
-    }
-
-    // Parse and set each cookie in the browser to allow future requests
-    for (const cookie of responseCookies) {
-      axios.defaults.headers.common["Cookie"] = responseCookies.join("; ");
-
-      const [cookieName, ...rest] = cookie.split("=");
-      const cookieValue = rest.join("=").split(";")[0];
-
-      (await cookies()).set(cookieName, cookieValue, {
-        sameSite: "lax",
-      });
-    }
-
-    const apiUser = loginResponse.data.user;
+    const response = await axios.post<ApiUser>("/auth/login", credentials);
+    const apiUser = response.data;
 
     return {
       id: apiUser.UserID.toString(),
@@ -61,11 +34,10 @@ export async function authorize(credentials: Partial<AuthorizeCredentials>) {
       roleName: apiUser.RoleName,
     };
   } catch (error) {
-    if (!(error instanceof AxiosError) || error.response?.status !== 422) {
-      return null; // Return null to display the default error message
+    if (!(error instanceof AxiosError) || error.response?.status !== 401) {
+      console.error(error);
     }
 
-    // In this case the error contains user-friendly error messages
-    throw new ApiResponseError(error.response.data.message);
+    return null;
   }
 }
