3e4bc48a2632c80d1a2c7d23d9226232d8a2817d
Rutger Pronk <rutger.pronk11@gmail.com>
2025-01-05 13:18:17 +0100

    Add timing to laravel requests in console.

--- front-end/src/lib/axios.ts
+++ front-end/src/lib/axios.ts
@@ -14,18 +14,41 @@ const axios = Axios.create({
   },
 });
 
+axios.interceptors.request.use((config) => {
+  console.log("Hi");
+
+  config.metadata = { startTime: new Date() };
+  return config;
+});
+
 axios.interceptors.response.use(
-  (response) => response,
+  (response) => {
+    const startTime: Date | undefined = response.config?.metadata?.startTime;
+    const endTime = new Date();
+
+    const duration = startTime
+      ? endTime.getTime() - startTime.getTime()
+      : undefined;
+
+    console.log(
+      ` Laravel ${response.config.method?.toUpperCase()} ${response.config.url} ${
+        response.status
+      } in ${duration}ms`,
+    );
+
+    return response;
+  },
   (error) => {
     if (error.response) {
       console.error("Axios Error with response:", error.response?.data.message);
-
       console.error("Axios Error Response:", {
         data: error.response.data,
       });
     } else {
       console.error("Axios Error without response:", error.message);
     }
+
+    return Promise.reject(error as Error);
   },
 );
 
