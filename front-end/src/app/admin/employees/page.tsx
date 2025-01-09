2e09c40ba2a1fa6b5c96153889448a6ddade4ecf
Rutger Pronk <rutger.pronk11@gmail.com>
2025-01-09 11:06:36 +0100

    Reduce gap

--- front-end/src/app/admin/employees/page.tsx
+++ front-end/src/app/admin/employees/page.tsx
@@ -2,11 +2,11 @@ import Link from "next/link";
 
 export default async function Employees() {
   return (
-    <main className="flex flex-col items-center justify-center gap-20">
+    <main className="flex flex-col items-center justify-center gap-10">
       <h1 className={"text-center text-3xl font-semibold"}>Medewerkers</h1>
       <Link
         href={"/admin/employees/register"}
-        className={"rounded bg-blue-500 p-3 text-white hover:opacity-90"}
+        className={"rounded bg-blue-500 px-3 py-2 text-white hover:opacity-90"}
       >
         Registreer nieuwe medewerker
       </Link>
