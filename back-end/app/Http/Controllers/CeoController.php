4c30bfc1c7c4e857e17f8658e5da57837eb400a4
ineedausernamehelp <54716860+JakevdKolk@users.noreply.github.com>
2025-01-15 14:08:03 +0100

    get all verlof from section

--- back-end/app/Http/Controllers/CeoController.php
+++ back-end/app/Http/Controllers/CeoController.php
@@ -4,19 +4,16 @@ namespace App\Http\Controllers;
 
 
 use App\Models\Sections;
-use App\Models\SectionUser;
 
 class CeoController extends Controller
 {
     public function showSection()
     {
-        $sections = Sections::all();
 
-        $leave = array();
-        foreach ($sections as $section) {
-            $leave[] = SectionUser::where('sectionId', $section->id)->with(['user.leave'])->get()->pluck('user.leave');
-        }
+        $leave = Sections::with('user.leave')->get();
 
         return response()->json($leave);
+
+
     }
 }
