fe3583824ff1ba0ed23adcbb5b68ce2f8b2b0970
Rutger Pronk <rutger.pronk11@gmail.com>
2025-01-16 23:42:11 +0100

    Allow parralel tests, write login tests

--- front-end/tests/login.spec.ts
+++ front-end/tests/login.spec.ts
@@ -1,11 +1,32 @@
-import { test } from "@playwright/test";
+import { expect, test } from "@playwright/test";
 
-test("test", async ({ page }) => {
-  await page.goto("/");
-  await page.getByLabel("Email").fill("employee@example.com");
-  await page.getByLabel("Wachtwoord").fill("secret");
-  await page.getByRole("button", { name: "Inloggen" }).click();
-  await page.getByRole("heading", { name: "Recente verlofverzoeken" }).click();
-  await page.getByRole("heading", { name: "Vakantiedagen" }).click();
-  await page.getByRole("heading", { name: "Verlofdagen" }).click();
+test.describe("Login", () => {
+  test("should successfully login and access dashboard the", async ({
+    page,
+  }) => {
+    await page.goto("/");
+
+    await page.getByLabel("Email").fill("employee@example.com");
+
+    await page.getByLabel("Wachtwoord").fill("secret");
+
+    await page.getByRole("button", { name: "Inloggen" }).click();
+
+    await expect(
+      page.getByRole("heading", { name: "Welcome Employee!" }),
+    ).toBeVisible();
+  });
+
+  test("should show error message with invalid credentials", async ({
+    page,
+  }) => {
+    await page.goto("/");
+
+    await page.getByLabel("Email").fill("invalid@example.com");
+    await page.getByLabel("Wachtwoord").fill("wrongpassword");
+    await page.getByRole("button", { name: "Inloggen" }).click();
+
+    // Check for error message
+    await expect(page.getByText("Ongeldige inloggegevens")).toBeVisible();
+  });
 });
