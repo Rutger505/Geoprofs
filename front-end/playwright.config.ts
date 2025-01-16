b933fa311f3bc9479ccf50e5a49121e5d71f96a4
Rutger Pronk <rutger.pronk11@gmail.com>
2025-01-16 17:11:42 +0100

    Ignore htts awrning

--- front-end/playwright.config.ts
+++ front-end/playwright.config.ts
@@ -28,6 +28,8 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: process.env.CI ? process.env.BASE_URL : "https://localhost",
 
+    ignoreHTTPSErrors: true,
+
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
   },
