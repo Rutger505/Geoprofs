62f86d24310860f4b76f1c88f4d75ab13b52bc3e
ineedausernamehelp <54716860+JakevdKolk@users.noreply.github.com>
2025-01-17 08:53:50 +0100

    made page fit layout

--- front-end/src/app/admin/sections/create/page.tsx
+++ front-end/src/app/admin/sections/create/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 import { createSection } from "@/app/admin/sections/create/create-section";
+import { Button, Field, Input, Label } from "@headlessui/react";
+import clsx from "clsx";
 import { useState } from "react";
 
 export default function Sections() {
@@ -22,16 +24,35 @@ export default function Sections() {
 
   return (
     <main className="flex flex-col items-center justify-center gap-10">
-      <form className="flex flex-col" onSubmit={handleSubmit}>
-        <label htmlFor="sectionName">Section Name</label>
-        <input
-          type="text"
-          placeholder="name"
-          name="sectionName"
-          value={sectionName}
-          onChange={(e) => setSectionName(e.target.value)} // Update state
-        />
-        <button type="submit">Submit</button>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          mutate();
+        }}
+        className="space-y-6"
+      >
+        <div className="flex max-w-sm flex-col gap-5">
+          <Field>
+            <Label className={"block"}>Sectie naam</Label>
+            <Input
+              className="w-full rounded-md border border-gray-300 px-3 py-2 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-500"
+              onChange={(event) => setSectionName(event.target.value)}
+              required
+            />
+          </Field>
+
+          <div className="space-y-4">
+            <Button
+              type="submit"
+              onSubmit={handleSubmit}
+              className={clsx(
+                "w-full rounded-md bg-blue-500 px-4 py-2 text-white transition-colors hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2",
+              )}
+            >
+              Registreer
+            </Button>
+          </div>
+        </div>
       </form>
     </main>
   );
