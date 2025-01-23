04a19063ce570540a493161bd155788ae7507ecd
Rutger Pronk <rutger.pronk11@gmail.com>
2025-01-23 09:58:37 +0100

    Create function to login as admin

--- tests/user.spec.ts
+++ tests/user.spec.ts
@@ -1,100 +1,74 @@
-const randomEmail = () => `test-${Math.random().toString(36).substring(2)}@example.com`;
-const randomString = () => `${Math.random().toString(36).substring(2)}`;
-
-import { expect, test } from "@playwright/test";
+async function loginAsAdmin(page) {
+ await page.goto("/");
+ await page.getByLabel("Email").fill("admin@example.com"); 
+ await page.getByLabel("Wachtwoord").fill("secret");
+ await page.getByRole("button", { name: "Inloggen" }).click();
+}
 
 test.describe("User", () => {
-  test("should be able to register user", async ({ page }) => {
-    // arrange
-    await page.goto("/");
+ test("should be able to register user", async ({ page }) => {
+   await loginAsAdmin(page);
 
-    // login
-    await page.getByLabel("Email").fill("admin@example.com");
-    await page.getByLabel("Wachtwoord").fill("secret");
-    await page.getByRole("button", { name: "Inloggen" }).click();
+   await page.getByRole("link", { name: "Werknemers" }).click();
+   await page.getByRole("link", { name: "Registreer nieuwe medewerker" }).click();
 
-    // navigate to register user page
-    await page.getByRole("link", { name: "Werknemers" }).click();
-    await page.getByRole("link", { name: "Registreer nieuwe medewerker" }).click();
+   await page.getByLabel("Voornaam").fill("Test");
+   await page.getByLabel("Achternaam").fill("user");
+   await page.getByLabel("Email").fill(randomEmail());
+   await page.getByLabel("Datum in dienst").fill("2025-01-24");
+   await page.getByLabel("Contracten").selectOption("3");
 
-    // act
-    // fill in form
-    await page.getByLabel("Voornaam").fill("Test");
-    await page.getByLabel("Achternaam").fill("user");
-    await page.getByLabel("Email").fill(randomEmail());
-    await page.getByLabel("Datum in dienst").fill("2025-01-24");
-    await page.getByLabel("Contracten").selectOption("3");
+   await page.getByRole("button", { name: "Registreer" }).click();
 
-    await page.getByRole("button", { name: "Registreer" }).click();
+   await expect(page.getByLabel("Rol")).toMatchAriaSnapshot(`
+   - combobox "Rol":
+     - option "CEO"
+     - option "Employee" [selected]
+     - option "Admin"
+     - option "SectionManager"
+     - option "ProjectManager"
+   `);
+   await expect(page.getByRole("paragraph")).toContainText("Medewerker geregistreerd!");
+ });
 
-    // assert
-    await expect(page.getByLabel("Rol")).toMatchAriaSnapshot(`
-    - combobox "Rol":
-      - option "CEO"
-      - option "Employee" [selected]
-      - option "Admin"
-      - option "SectionManager"
-      - option "ProjectManager"
-    `);
-    await expect(page.getByRole("paragraph")).toContainText("Medewerker geregistreerd!");
-  });
+ test("should change user last name", async ({ page }) => {
+   await loginAsAdmin(page);
 
-  test("should change user last name", async ({ page }) => {
-    // arrange
-    await page.goto("/");
+   await page.getByRole("link", { name: "Werknemers" }).click();
+   await page.locator("div").filter({ hasText: /^Employee Example$/ }).getByRole("link").click();
 
-    // login
-    await page.getByLabel("Email").fill("admin@example.com");
-    await page.getByLabel("Wachtwoord").fill("secret");
-    await page.getByRole("button", { name: "Inloggen" }).click();
+   const newName = randomString();
+   await page.getByLabel("Achternaam").fill(newName);
 
-    // navigate to users page
-    await page.getByRole("link", { name: "Werknemers" }).click();
+   await page.locator("form").filter({ hasText: "EmailVoornaamAchternaamOpslaan" }).getByRole("button").click();
+   await expect(page.locator("h1")).toContainText(newName);
 
+   await page.getByLabel("Achternaam").fill("Example");
+   await page.locator("form").filter({ hasText: "EmailVoornaamAchternaamOpslaan" }).getByRole("button").click();
+   await expect(page.locator("h1")).toContainText("Example");
+ });
 
-    await page.locator("div").filter({ hasText: /^Employee Example$/ }).getByRole("link").click();
+ test("should save section selection", async ({ page }) => {
+   await loginAsAdmin(page);
 
-    const newName = randomString();
-    await page.getByLabel("Achternaam").fill(newName);
+   await page.getByRole("link", { name: "Werknemers" }).click();
+   await page.locator("div").filter({ hasText: /^Employee Example$/ }).getByRole("link").click();
 
-    await page.locator("form").filter({ hasText: "EmailVoornaamAchternaamOpslaan" }).getByRole("button").click();
+   await page.getByLabel("Sectie").selectOption("1");
+   await page.getByRole("button", { name: "Opslaan" }).first().click();
 
-    await expect(page.locator("h1")).toContainText(newName);
+   await expect(page.getByLabel("Sectie")).toHaveValue("1");
+ });
 
-    // reset
-    await page.getByLabel("Achternaam").fill("Example");
-    await page.locator("form").filter({ hasText: "EmailVoornaamAchternaamOpslaan" }).getByRole("button").click();
+ test("should save project selection", async ({ page }) => {
+   await loginAsAdmin(page);
 
-    await expect(page.locator("h1")).toContainText("Example");
-  });
+   await page.getByRole("link", { name: "Werknemers" }).click();
+   await page.locator("div").filter({ hasText: /^Employee Example$/ }).getByRole("link").click();
 
-  test("should save section selection", async ({ page }) => {
-    await page.goto("/");
-    await page.getByLabel("Email").fill("admin@example.com");
-    await page.getByLabel("Wachtwoord").fill("secret");
-    await page.getByRole("button", { name: "Inloggen" }).click();
+   await page.getByLabel("Project").selectOption("1"); 
+   await page.getByRole("button", { name: "Opslaan" }).nth(1).click();
 
-    await page.getByRole("link", { name: "Werknemers" }).click();
-    await page.locator("div").filter({ hasText: /^Employee Example$/ }).getByRole("link").click();
-
-    await page.getByLabel("Sectie").selectOption("1");
-    await page.getByRole("button", { name: "Opslaan" }).first().click();
-
-    await expect(page.getByLabel("Sectie")).toHaveValue("1");
-  });
-
-  test("should save project selection", async ({ page }) => {
-    await page.goto("/");
-    await page.getByLabel("Email").fill("admin@example.com");
-    await page.getByLabel("Wachtwoord").fill("secret");
-    await page.getByRole("button", { name: "Inloggen" }).click();
-
-    await page.getByRole("link", { name: "Werknemers" }).click();
-    await page.locator("div").filter({ hasText: /^Employee Example$/ }).getByRole("link").click();
-
-    await page.getByLabel("Project").selectOption("1");
-    await page.getByRole("button", { name: "Opslaan" }).nth(1).click();
-
-    await expect(page.getByLabel("Project")).toHaveValue("1");
-  });
+   await expect(page.getByLabel("Project")).toHaveValue("1");
+ });
 });
