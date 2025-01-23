f85f783872821f36f913c2bbc9a89228c4dfa8fc
ineedausernamehelp <54716860+JakevdKolk@users.noreply.github.com>
2025-01-23 16:03:45 +0100

    cleaned up unused code

--- back-end/app/Models/Leave.php
+++ back-end/app/Models/Leave.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Leave extends Model
 {
@@ -27,8 +26,4 @@ class Leave extends Model
         return $this->belongsTo(LeaveCategory::class, 'categoryId', 'id');
     }
 
-    public function user(): HasMany
-    {
-        return $this->hasMany(User::class, 'id', 'userId');
-    }
 }
