26fec6c91f731ddcf70db6b2d590b123d1bd35f4
Rutger Pronk <rutger.pronk11@gmail.com>
2025-01-28 11:53:08 +0100

    Improve validation

--- front-end/src/components/SectionManager/Calendar/SectionManagerCalendar.tsx
+++ front-end/src/components/SectionManager/Calendar/SectionManagerCalendar.tsx
@@ -19,6 +19,11 @@ export async function SectionManagerCalendar() {
   }
 
   const userSection = await getUserSection(session.user.id);
+
+  if (!userSection) {
+    return <div>Je bent geen gecoppeld aan een sectie</div>;
+  }
+
   const users = await getUsersInSection(userSection.id);
 
   const allLeaveRequests = await getSectionManagerLeaveRequests(
