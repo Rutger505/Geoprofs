acb65a2b9ac485aacfb5e3b34fdde8f203b3e372
Rutger <119070855+Rutger505@users.noreply.github.com>
2025-01-28 11:50:16 +0100

    Update front-end/src/app/(protected)/project-manager/project-calendar/page.tsx

--- front-end/src/app/(protected)/project-manager/project-calendar/page.tsx
+++ front-end/src/app/(protected)/project-manager/project-calendar/page.tsx
@@ -3,7 +3,7 @@ import { ProjectManagerCalendar } from "@/components/ProjectManager/Calendar/Pro
 export default async function LeaveRequests() {
   return (
     <main className="flex flex-col items-center gap-14 px-3 py-20 md:px-10">
-      <h1 className={"text-3xl font-semibold"}>Verlofverzoeken</h1>
+      <h1 className={"text-3xl font-semibold"}>Project kalender</h1>
 
       <ProjectManagerCalendar />
     </main>
