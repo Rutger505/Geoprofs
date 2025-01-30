979fba97f25d7297d9ef33bcc57cf5eaf502b3d7
ineedausernamehelp <54716860+JakevdKolk@users.noreply.github.com>
2025-01-30 11:10:35 +0100

    fixed typescript issue

--- front-end/src/app/(protected)/admin/contracts/create/page.tsx
+++ front-end/src/app/(protected)/admin/contracts/create/page.tsx
@@ -47,7 +47,9 @@ export default function Contracts() {
             <Label className="block">Contract total leave hours</Label>
             <Input
               className="w-full rounded-md border border-gray-300 px-3 py-2 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-500"
-              onChange={(event) => setContractLeaveHours(event.target.value)}
+              onChange={(event) =>
+                setContractLeaveHours(Number(event.target.value))
+              }
               value={contractLeaveHours}
               required
             />
