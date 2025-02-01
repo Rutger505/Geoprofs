addbd7492df1c5c28acd32e3595c18148a67607a
JakeKolk <54716860+JakevdKolk@users.noreply.github.com>
2025-02-01 12:30:40 +0100

    Update LeaveTest.php
    nog een test verwijderd

--- back-end/tests/Feature/LeaveTest.php
+++ back-end/tests/Feature/LeaveTest.php
@@ -86,28 +86,6 @@ class LeaveTest extends TestCase
     }
 
 
-    public function test_update_already_accepted_leave(): void
-    {
-
-        $leaveId = 1;
-        // Sample login credentials
-        $credentials = [
-            'status' => 'accepted',
-
-        ];
-
-        // Send a POST request with credentials to the login endpoint
-        $response = $this->putJson("api/leave/$leaveId", $credentials);
-
-        // Assert the response status is 200 (OK)
-        $response->assertStatus(404);
-
-        // Assert the response body contains specific structure or data
-        $response->assertJsonStructure([
-            'message',
-        ]);
-    }
-
     public function test_update_non_existent_leave(): void
     {
         $leaveId = 0;
